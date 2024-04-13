5cc37e659073bf600ac755ade0c2364e63de96c3
Yen Minh <minhnguyenchauyen@gmail.com>
2024-04-13 10:29:21 +0700

    btvn

---
+++ Lab/Lab 1/main.js
@@ -0,0 +1,15 @@
+
+        var fruitbasket = [["Apple",10], ["Grape",15], ["Orange",5]]
+        console.log('I have 15 grapes',fruitbasket[1]);
+        console.log('I have 10 apples',fruitbasket[0]);
+        console.log('I have 5 oranges',fruitbasket[2]);
+
+        var Husky = {
+            'name': 'Ngáo',
+            'legs': 4,
+            'color': 'Brown',
+        }
+        console.log(Husky);
+
+    
+
